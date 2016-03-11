76fbf88c4c4af767ee48b8bbdb2840a3f231f7a3
Peter Fitzpatrick <petarfitzpatrick@gmail.com>
2016-03-11 18:20:27 -0500

    no message

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,2 +1,26 @@
-"use strict"
-
+		"use strict";
+		window.onload = loadData;
+		
+		var influencees = [];
+		
+		function loadData(){
+			var xhr = new XMLHttpRequest();
+			xhr.onload = function(){ 
+				// JSON.parse() converts a string to JSON. 
+				var myJSON = JSON.parse( xhr.responseText );
+				var allInfluencees = myJSON.influencees; 
+				for (var i=0; i< allInfluencees.length;i++){ 
+					
+					influencees[i] = allInfluencees[i];
+					
+					var influencee = allInfluencees[i]; 
+					
+					console.log(influencee.artist);
+					console.log(influencee.genre);
+					console.log(influencee.influencers);
+				} 
+			}
+			var url = "scripts/json/tempArtists.json";
+			xhr.open('GET',url,true);
+			xhr.send();
+		}
