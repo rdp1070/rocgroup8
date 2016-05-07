139e116db1da159e1e935f0d2411f3c1f0da79b1
rdp1070 <rdp1070@rit.edu>
2016-05-06 23:09:19 -0400

    Add more files

---
+++ end.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>title</title>
+        <link rel="stylesheet" href="styles/style.css">
+        <title>Rocpocalypse Project</title>
+        <script src="end.js"></script>
+    </head>
+    <body>
+        <!-- page content -->
+        <section id="content">
+
+        </section>
+    </body>
+</html>
